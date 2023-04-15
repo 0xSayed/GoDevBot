21494ad1680d4544d97e76a204461479c74ff16e
Risker | ريسكر <67011779+iRiskRisker@users.noreply.github.com>
2023-04-15 13:00:26 +0300

    Initial commit

---
+++ bot/launch.js
@@ -0,0 +1,8 @@
+const { DiscordBot } = require("@disqada/halfbot");
+
+/** @type { import("@disqada/halfbot").DiscordBotData } */
+const botData = {
+	rootDirectory: "bot"
+};
+
+new DiscordBot(botData);
