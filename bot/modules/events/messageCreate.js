debfa614f9bc0d5862f547ac0bcfa16860557043
The Alpha <contact@nabilalsaiad.com>
2023-04-18 14:36:56 +0800

    Add auto publish

---
+++ bot/modules/events/messageCreate.js
@@ -0,0 +1,22 @@
+const { Events, ChannelType } = require("discord.js");
+const { BotEvent } = require("@disqada/halfbot");
+
+/** @type { import("@disqada/halfbot").BotEventData } */
+const data = {
+	name: Events.MessageCreate
+};
+
+/**
+ * @param { import("@disqada/halfbot").DiscordBot } bot
+ * @param { import("discord.js").Message } message
+ */
+async function execute(bot, message) {
+	if (
+		message.channel.type === ChannelType.GuildAnnouncement &&
+		message.crosspostable
+	) {
+		await message.crosspost();
+	}
+}
+
+module.exports = new BotEvent(data, execute);
